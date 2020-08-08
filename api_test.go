2ff91a75dbd04875353e6bda9b31168333fd5399
nathanwong37 <50334967+nathanwong37@users.noreply.github.com>
2020-08-08 00:10:14 -0700

    a

--- api_test.go
+++ api_test.go
@@ -12,13 +12,13 @@ func TestRun(t *testing.T) {
 	// conf := memberlist.DefaultLocalConfig()
 	// test := NewMessenger(conf)
 	nodes := []string{
-		"localhost:7946",
+		"localhost:8310",
 	}
 	// test.Join(nodes)
 	conf2 := memberlist.DefaultWANConfig()
 	// conf2.Name = "NotFeelingLucky"
-	conf2.BindPort = 8301
-	conf2.AdvertisePort = 8301
+	conf2.BindPort = 2134
+	conf2.AdvertisePort = 2134
 	test2 := NewMessenger(conf2)
 	fmt.Println(test2.M.LocalNode().Address())
 	test2.Join(nodes)
